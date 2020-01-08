23cdf60f52c89af45842f23f8647248f2f85875f
xkylesun <xyasun@gmail.com>
2020-01-07 17:57:09 -0800

    bug fix

--- config/db/seeds.rb
+++
@@ -1,4 +0,0 @@
-User.create!([
-  {name: "test01", email: "test01@email.com", password_digest: "$2a$12$DCiB86s5iXq.WzEc0pa6Q.wanvlVlk2NuTTpGcuY3ACGo6XR4I1jO", session_token: "PYknayNBsOyLqoyST345kw", biography: nil, avatar: nil},
-  {name: "test02", email: "test02@email.com", password_digest: "$2a$12$GAz1Z7gRdyZBBMh/5q3zGeA2r7gqvm2tGUWKzGMcQ00gPWyCyJ4le", session_token: "Nb2RPxXsigxskrW-htfyqQ", biography: nil, avatar: nil}
-])
