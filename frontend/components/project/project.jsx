d77c2b4fe4c8cead69960960e1e213c621513eaa
xkylesun <xyasun@gmail.com>
2020-01-10 09:50:10 -0800

    yeeeeeeee

--- frontend/components/project/project.jsx
+++ frontend/components/project/project.jsx
@@ -13,28 +13,44 @@ export default class Project extends React.Component{
         }
     }
 
+    componentDidUpdate(prevProps) {
+        // console.log(this.props.match.params.projectId);
+        // console.log(prevProps.id)
+        if (this.props.match.params.projectId !== prevProps.id) {
+            this.props.fetchProject(this.props.match.params.projectId)
+            // setTimeout(() => this.setState({
+            //         title: this.props.project.title,
+            //         body: this.props.project.body,
+            //         videoLink: this.props.project.videoLink,
+            //         pledgeLevels: this.props.pledgeLevels
+            //     }), 1000)
+        }
+    }
+
     componentDidMount(){
         this.props.fetchProject(this.props.match.params.projectId)
-        setTimeout(()=> console.dir(this.props),1000)
-        setTimeout(() => this.setState({
-            title: this.props.project.title,
-            body: this.props.project.body,
-            videoLink: this.props.project.videoLink,
-            pledgeLevels: this.props.pledgeLevels
-        }), 1000)
+        // setTimeout(()=> console.dir(this.props),1000)
+        // setTimeout(() => this.setState({
+        //     title: this.props.project.title,
+        //     body: this.props.project.body,
+        //     videoLink: this.props.project.videoLink,
+        //     pledgeLevels: this.props.pledgeLevels
+        // }), 1000)
     }
 
     render(){
         if (!this.props.project) return null;
+        const {title, body, videoLink} = this.props.project;
+        
         return (
         <div>
             PROJECT SHOW PAGE
             <video src=""></video>
-            <section>{this.state.body}</section>
-            <p>{this.state.title}</p>
+            <section>{body}</section>
+            <p>{title}</p>
             <section className="pledge-level-frame">
                 <ul className="pledge-levels">
-                    {this.state.pledgeLevels.map(level => <PledgeLevel level={level} key={level.id}/>)}
+                    {this.props.pledgeLevels.map(level => <PledgeLevel level={level} key={level.id}/>)}
                 </ul>
             </section>
 
